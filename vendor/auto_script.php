dc370d091b2832f880f347843ed5609d79b97892
K1ra-Python <makismka.guchenko@mail.ru>
2024-03-26 23:14:56 +0300

    add session_message

--- vendor/auto_script.php
+++ vendor/auto_script.php
@@ -10,9 +10,7 @@ $sql->execute([$login]);
 $user = $sql->fetch(PDO::FETCH_ASSOC);
 
 if ($user && password_verify($pass, $user['PASSWORD'])) {
-    // Пароль верифицирован, аутентификация прошла успешно
-    // Устанавливаем сессионную переменную, например, user_id или login
-    // Например: $_SESSION['user_login'] = $login; 
+    $_SESSION['user_login'] = $login; 
     // Это будет указывать на то, что пользователь вошел в систему
     // Удаляем сообщение об ошибке, если оно было установлено ранее
     unset($_SESSION['ERROR_MESSAGE']);
